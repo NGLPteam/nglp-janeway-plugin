e15e22464d9574ed52c0ce19a404ffd3bc14171f
Dhileas Heywood <dhileas.heywood@gmail.com>
2021-08-10 15:58:21 +0100

    request event can be sent to event api

--- apps.py
+++ apps.py
@@ -1,6 +1,12 @@
+import json
+import logging
+
 from django import apps
 from django.core import signals
+import requests
+import requests.exceptions
 
+logger = logging.getLogger(__name__)
 
 
 class NGLPConfig(apps.AppConfig):
@@ -10,7 +16,18 @@ class NGLPConfig(apps.AppConfig):
     @property
     def analytics_url(self):
         from django.conf import settings
-        return settings.NGLP_ANALYTICS_URL
+        return settings.NGLP_ANALYTICS_API
 
     def ready(self):
         from utils.models import LogEntry
+
+    def send_event(self, event):
+        # put event into JSON
+        # POST request
+        # find api endpoint
+
+        try:
+            response = requests.post(self.analytics_url, data=json.dumps(event))
+            response.raise_for_status()
+        except requests.exceptions.RequestException:
+            logger.exception(msg="failed to send event.")
--- middleware.py
+++ middleware.py
@@ -1,11 +1,14 @@
 from http import HTTPStatus
 
+from django.apps import apps
 from django.http import HttpRequest, HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 from django.core.urlresolvers import ResolverMatch
 
 from core import models as core_models
 from submission import models as submission_models
+
+
 
 
 class NGLPMiddleware(MiddlewareMixin):
@@ -19,17 +22,19 @@ class NGLPMiddleware(MiddlewareMixin):
             article: submission_models.Article = file.article
 
             event = {
-                'event': 'Request',
+                'event': 'request',
                 'object_type': 'File',
                 'object_id': list(filter(None, [article.get_identifier('pubid'), article.get_doi()])),
                 'format': file.mime_type,
                 'url': request.build_absolute_uri(),
                 'referrer': request.META.get('HTTP_REFERRER'),
                 'user_agent': request.META.get('HTTP_USER_AGENT'),
+                'ip' : request.META.get('REMOTE_ADDR')
                 # FIXME: Talk to Richard about how pseudoanonymised he wants the IPs to be, and when
 
             }
 
             # TODO: Send event to API
+            apps.get_app_config("nglp").send_event(event)
 
         return response
